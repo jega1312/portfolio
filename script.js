b55ae10e8c1aba287b03015c70c8990b1093c560
Jegathiswaran Thiagu <thiagu1312@gmail.com>
2025-06-22 22:17:59 +0800

    Update style

--- script.js
+++ script.js
@@ -2,26 +2,28 @@ let hamburger = document.querySelector(".hamburger");
 
 let navList = document.querySelector(".nav-list");
 
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navList.classList.toggle("active");
+});
 
-hamburger.addEventListener("click", function(){
-    hamburger.classList.toggle("active");
-    navList.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-list").forEach(n => n.addEventListener("click", function () {
+document.querySelectorAll(".nav-list").forEach((n) =>
+  n.addEventListener("click", function () {
     hamburger.classList.remove("active");
     navList.classList.remove("active");
-}))
-
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwGwIOdqcMcphbKq_h6zztNkJ79OYrSqFb8rxHaECJztRyU5ipFJ9-vkYLAqh9yxqLN/exec'
-  const form = document.forms['submit-to-google-sheet']
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        alert("Thank you for your submission.")
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
   })
+);
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwGwIOdqcMcphbKq_h6zztNkJ79OYrSqFb8rxHaECJztRyU5ipFJ9-vkYLAqh9yxqLN/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      alert("Thank you for your submission.");
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
